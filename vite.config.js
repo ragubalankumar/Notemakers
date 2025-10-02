4a2982b6164a77071d607343435ac850f7a0d864
Ragubalan kumar <ragubalan360@gmail.com>
2025-10-02 11:03:37 +0530

    Resolve vite.config.js merge conflict with react + tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  root: './', // optional, defaults to project root
-});
-=======
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
->>>>>>> 7271d0a7ef74e53aebe1f767a8009ad6ea7b4101
