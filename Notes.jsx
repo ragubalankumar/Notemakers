a7f21b1174a2b911c7e96dd316a7b1c3e6242f8f
Ragu Balan <155983587+ragubalankumar@users.noreply.github.com>
2026-04-08 20:40:08 +0530

    fix: correct VideoCapture import case in Notes.jsx

--- Notes.jsx
+++ Notes.jsx
@@ -7,7 +7,7 @@ import Sidebar from "../UI/Sidebar";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import VideoCapture from "./src/Pages/Videocapture";
+import VideoCapture from "./src/Pages/VideoCapture";
 
 const BASE_URL = "http://localhost:5001";
 
